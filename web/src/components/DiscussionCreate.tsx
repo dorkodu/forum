ce4dddffedf0d1d2f8754bc44271e6979a38217e
Berk Cambaz <berkcambaz12321@gmail.com>
2023-01-15 18:08:48 +0300

    Update discussion create component & home route

--- web/src/components/DiscussionCreate.tsx
+++ web/src/components/DiscussionCreate.tsx
@@ -1,7 +1,28 @@
+import { useState } from "react"
+
 function DiscussionCreate() {
+  const [discussion, setDiscussion] = useState({ title: "", readme: "" })
+
   return (
     <>
-      create
+      <input
+        type="text"
+        placeholder="title"
+        defaultValue={discussion.title}
+        onChange={(ev) => { setDiscussion({ ...discussion, title: ev.target.value }) }}
+      />
+
+      <br />
+
+      <textarea
+        placeholder="readme"
+        defaultValue={discussion.readme}
+        onChange={(ev) => { setDiscussion({ ...discussion, readme: ev.target.value }) }}
+      ></textarea>
+
+      <br />
+
+      <button>create</button>
     </>
   )
 }
--- web/src/routes/Home.tsx
+++ web/src/routes/Home.tsx
@@ -1,9 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import DiscussionSummary from "../components/DiscussionSummary";
 
 function Home() {
+  const navigate = useNavigate();
+
   return (
     <>
       <div>home</div>
+      <button onClick={() => { navigate("/create-discussion") }}>create discussion</button>
 
       {[...Array(5)].map((_v, i) => <div key={i}><hr /><DiscussionSummary /></div>)}
     </>
