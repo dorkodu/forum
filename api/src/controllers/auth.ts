b4b132cb22c41f37c867e4bf696a074ef3250efb
Berk Cambaz <berkcambaz12321@gmail.com>
2023-01-15 13:45:59 +0300

    Save user to database on first login

--- api/src/controllers/auth.ts
+++ api/src/controllers/auth.ts
@@ -7,6 +7,8 @@ import { ErrorCode } from "../types/error_codes";
 import { getAccessTokenSchema } from "../schemas/auth"
 import { config } from "../config";
 import { date } from "../lib/date";
+import pg from "../pg";
+import { util } from "../lib/util";
 
 async function middleware(ctx: SchemaContext) {
   const rawToken = token.get(ctx.req);
@@ -36,6 +38,28 @@ const getAccessToken = sage.resource(
 
     const accessToken = await queryGetAccessToken(parsed.data.code);
     if (!accessToken) return { error: ErrorCode.Default };
+
+    const userData = await queryUserData(accessToken.token);
+    if (!userData) return { error: ErrorCode.Default };
+
+    // Check if account with the given user id already exists
+    const [result0]: [{ count: string }?] = await pg`SELECT COUNT(*) FROM users WHERE id=${userData.id}`
+    if (!result0) return { error: ErrorCode.Default };
+
+    // If first time logging in via Dorkodu ID, create an account for the user
+    if (util.intParse(result0.count, 1) === 0) {
+      const row = {
+        id: userData.id,
+        name: userData.username,
+        username: userData.username,
+        joined_at: date.utc(),
+        follower_count: 0,
+        following_count: 0,
+      }
+
+      const result = await pg`INSERT INTO users ${pg(row)}`;
+      if (result.count === 0) return { error: ErrorCode.Default };
+    }
 
     // Attach the access token for 30 days to the user
     token.attach(ctx.res, { value: accessToken.token, expiresAt: date.day(30) });
@@ -91,6 +115,37 @@ async function queryGetAccessToken(code: string): Promise<{ token: string } | un
         axios.post(
           "https://id.dorkodu.com/api",
           { a: { res: "getAccessToken", arg: { code } } }
+        )
+          .then((value) => { resolve(value.data.a.data) })
+          .catch((_reason) => { resolve(undefined) })
+        break;
+      default: resolve(undefined);
+    }
+  })
+}
+
+interface UserData {
+  id: string;
+  username: string;
+  email: string;
+  joinedAt: number;
+}
+
+async function queryUserData(token: string): Promise<UserData | undefined> {
+  return new Promise((resolve) => {
+    switch (config.env) {
+      case "development":
+        axios.post(
+          "http://id_api:8001/api",
+          { a: { res: "getUserData", arg: { token } } }
+        )
+          .then((value) => { resolve(value.data.a.data) })
+          .catch((_reason) => { resolve(undefined) })
+        break;
+      case "production":
+        axios.post(
+          "https://id.dorkodu.com/api",
+          { a: { res: "getUserData", arg: { token } } }
         )
           .then((value) => { resolve(value.data.a.data) })
           .catch((_reason) => { resolve(undefined) })
