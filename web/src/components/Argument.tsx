930710d3f63921ecfb3fb9a8e038570319b77042
Berk Cambaz <berkcambaz12321@gmail.com>
2023-01-15 15:51:43 +0300

    Implement argument and discussion components

--- web/src/components/Argument.tsx
+++ web/src/components/Argument.tsx
@@ -1,6 +1,26 @@
+import { useState } from "react"
+
 function Argument() {
+  const [vote, setVote] = useState<"none" | "up" | "down">("none")
   return (
-    <>argument</>
+    <>
+      <div>
+        <span>Berk Cambaz</span>
+        &nbsp;
+        <span>@berkcambaz</span>
+        &nbsp;
+        <span>16h</span>
+      </div>
+      <div>hello, world!</div>
+      <div>
+        <span>votes: 123</span>
+        &nbsp;
+        <button onClick={() => { setVote("up") }}>upvote</button>
+        <button onClick={() => { setVote("down") }}>downvote</button>
+        &nbsp;
+        <span>{vote}</span>
+      </div>
+    </>
   )
 }
 
--- web/src/components/Discussion.tsx
+++ web/src/components/Discussion.tsx
@@ -1,8 +1,15 @@
 import { useState } from "react"
+import Argument from "./Argument";
 import Comment from "./Comment"
 
 function Discussion() {
   const [showing, setShowing] = useState<"comments" | "arguments">("arguments");
+  const [time, setTime] = useState<"asc" | "desc">("desc");
+  const [type, setType] = useState<"any" | "positive" | "negative">("any");
+  const [votes, setVotes] = useState<"any" | "most" | "least">("any");
+
+  const [comment, setComment] = useState({ text: "" });
+  const [argument, setArgument] = useState({ text: "", type: "+" as "+" | "-" });
 
   return (
     <>
@@ -12,12 +19,20 @@ function Discussion() {
         <span>@berkcambaz</span>
         &nbsp;
         <span>16h</span>
+        <div>is milk white or black?</div>
+        <div>
+          <span>arguments: 123</span>
+          &nbsp;
+          <span>comments: 123</span>
+        </div>
       </div>
 
       <hr />
 
       <div>
         readme
+        <br />
+        so the milk is cow's product and it's...
       </div>
 
       <hr />
@@ -28,22 +43,30 @@ function Discussion() {
         <br />
         {showing === "comments" &&
           <>
-            <button>time asc</button>
-            <button>time desc</button>
+            <button onClick={() => { setTime("asc") }}>time asc</button>
+            <button onClick={() => { setTime("desc") }}>time desc</button>
+            &nbsp;
+            <span>{time}</span>
           </>
         }
         {showing === "arguments" &&
           <>
-            <button>time asc</button>
-            <button>time desc</button>
+            <button onClick={() => { setTime("asc") }}>time asc</button>
+            <button onClick={() => { setTime("desc") }}>time desc</button>
+            &nbsp;
+            <span>{time}</span>
             <br />
-            <button>show all</button>
-            <button>show positive</button>
-            <button>show negative</button>
+            <button onClick={() => { setType("any") }}>show all</button>
+            <button onClick={() => { setType("positive") }}>show positive</button>
+            <button onClick={() => { setType("negative") }}>show negative</button>
+            &nbsp;
+            <span>{type}</span>
             <br />
-            <button>any votes</button>
-            <button>most votes</button>
-            <button>least votes</button>
+            <button onClick={() => { setVotes("any") }}>any votes</button>
+            <button onClick={() => { setVotes("most") }}>most votes</button>
+            <button onClick={() => { setVotes("least") }}>least votes</button>
+            &nbsp;
+            <span>{votes}</span>
           </>
         }
       </div>
@@ -52,7 +75,39 @@ function Discussion() {
 
       <div>showing {showing}</div>
 
-      {[...Array(5)].map((_v, i) => <div key={i}><hr /><Comment /></div>)}
+      <hr />
+
+      {showing === "arguments" &&
+        <>
+          <input
+            type="text"
+            placeholder="write argument..."
+            defaultValue={argument.text}
+            onChange={(ev) => { setArgument({ ...argument, text: ev.target.value }) }}
+          />
+          <button onClick={() => { setArgument({ ...argument, type: "+" }) }}>+</button>
+          <button onClick={() => { setArgument({ ...argument, type: "-" }) }}>-</button>
+          &nbsp;
+          <span>type: {argument.type}</span>
+          <br />
+          <button>send</button>
+        </>
+      }
+      {showing === "comments" &&
+        <>
+          <input
+            type="text"
+            placeholder="write comment..."
+            defaultValue={comment.text}
+            onChange={(ev) => { setComment({ ...comment, text: ev.target.value }) }}
+          />
+          <br />
+          <button>send</button>
+        </>
+      }
+
+      {showing === "arguments" && [...Array(5)].map((_v, i) => <div key={i}><hr /><Argument /></div>)}
+      {showing === "comments" && [...Array(5)].map((_v, i) => <div key={i}><hr /><Comment /></div>)}
     </>
   )
 }
