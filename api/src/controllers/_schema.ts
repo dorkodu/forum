1fa3c43635741158e75bbec72387f9fd40d63e6f
Berk Cambaz <berkcambaz12321@gmail.com>
2023-01-16 16:05:33 +0300

    Add sage resources to the schema

--- api/src/controllers/_schema.ts
+++ api/src/controllers/_schema.ts
@@ -2,6 +2,8 @@ import sage from "@dorkodu/sage-server";
 import { NextFunction, Request, Response } from "express";
 
 import auth from "./auth";
+import user from "./user";
+import discussion from "./discussion";
 
 export interface SchemaContext {
   readonly req: Request;
@@ -19,5 +21,36 @@ export const schema = sage.schema(
     /* auth */
     auth: auth.auth,
     getAccessToken: auth.getAccessToken,
+
+    /* user */
+    getUser: user.getUser,
+    editUser: user.editUser,
+    searchUser: user.searchUser,
+
+    getUserDiscussions: user.getUserDiscussions,
+
+    followUser: user.followUser,
+    getUserFollowers: user.getUserFollowers,
+    getUserFollowing: user.getUserFollowing,
+
+    /* discussion */
+    createDiscussion: discussion.createDiscussion,
+    deleteDiscussion: discussion.deleteDiscussion,
+    editDiscussion: discussion.editDiscussion,
+    searchDiscussion: discussion.searchDiscussion,
+
+    favouriteDiscussion: discussion.favouriteDiscussion,
+
+    getUserDiscussionFeed: discussion.getUserDiscussionFeed,
+    getGuestDiscussionFeed: discussion.getGuestDiscussionFeed,
+
+    createComment: discussion.createComment,
+    deleteComment: discussion.deleteComment,
+    getComments: discussion.getComments,
+
+    createArgument: discussion.createArgument,
+    deleteArgument: discussion.deleteArgument,
+    getArguments: discussion.getArguments,
+    voteArgument: discussion.voteArgument,
   }
 )
