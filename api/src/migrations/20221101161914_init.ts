a0d13f598decf54d7f2ef4833ce1de83be73ab53
Berk Cambaz <berkcambaz12321@gmail.com>
2023-01-16 14:45:26 +0300

    Add bio to users schema

--- api/src/migrations/20221101161914_init.ts
+++ api/src/migrations/20221101161914_init.ts
@@ -6,6 +6,7 @@ export async function up(knex: Knex): Promise<void> {
       table.bigint("id").primary()
       table.string("name", 64)
       table.string("username", 16).unique()
+      table.string("bio", 500)
       table.bigint("joined_at")
       table.bigint("follower_count")
       table.bigint("following_count")
