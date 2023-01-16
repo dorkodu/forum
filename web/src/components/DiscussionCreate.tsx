19e6f0e34c06fa2f5d591ee69af3f4573a8e6cb3
Berk Cambaz <berkcambaz12321@gmail.com>
2023-01-16 19:08:18 +0300

    Update DiscussionCreate.tsx

--- web/src/components/DiscussionCreate.tsx
+++ web/src/components/DiscussionCreate.tsx
@@ -4,6 +4,8 @@ import { useDiscussionStore } from "../stores/discussionStore";
 interface State {
   title: string;
   readme: string;
+  loading: boolean;
+  status: boolean | undefined;
 }
 
 function DiscussionCreate() {
@@ -17,16 +19,19 @@ function DiscussionCreate() {
       newDiscussion.title = newDiscussion.title.substring(0, 100000);
 
     return newDiscussion;
-  }, { title: "", readme: "" })
+  }, { title: "", readme: "", loading: false, status: undefined })
 
   const queryCreateDiscussion = useDiscussionStore(state => state.queryCreateDiscussion);
 
   const createDiscussion = async () => {
     if (discussion.title.length === 0) return;
+    if (discussion.title.length > 100) return;
     if (discussion.readme.length === 0) return;
+    if (discussion.readme.length > 100000) return;
 
+    setDiscussion({ ...discussion, loading: true, status: undefined });
     const status = await queryCreateDiscussion(discussion.title, discussion.readme);
-    console.log(status);
+    setDiscussion({ ...discussion, loading: false, status: status });
   }
 
   return (
@@ -34,6 +39,7 @@ function DiscussionCreate() {
       <input
         type="text"
         placeholder="title"
+        disabled={discussion.loading}
         defaultValue={discussion.title}
         onChange={(ev) => { setDiscussion({ ...discussion, title: ev.target.value }) }}
       />
@@ -42,13 +48,23 @@ function DiscussionCreate() {
 
       <textarea
         placeholder="readme"
+        disabled={discussion.loading}
         defaultValue={discussion.readme}
         onChange={(ev) => { setDiscussion({ ...discussion, readme: ev.target.value }) }}
       ></textarea>
 
       <br />
 
-      <button onClick={createDiscussion}>create</button>
+      <button
+        disabled={discussion.loading}
+        onClick={createDiscussion}
+      >
+        create
+      </button>
+
+      {discussion.loading ? <><br />loading...</> : ""}
+      {discussion.status === true && <><br />success...</>}
+      {discussion.status === false && <><br />fail...</>}
     </>
   )
 }
