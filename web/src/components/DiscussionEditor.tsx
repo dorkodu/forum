0b251e898b49736959113fd26ecf7bdd2103e5cf
Berk Cambaz <berkcambaz12321@gmail.com>
2023-01-26 13:42:28 +0300

    Implement discussion editor UI

--- web/src/components/DiscussionEditor.tsx
+++ web/src/components/DiscussionEditor.tsx
@@ -1,3 +1,4 @@
+import { Button, Card, Textarea, TextInput } from "@mantine/core";
 import { useEffect, useReducer } from "react"
 import { request, sage } from "../stores/api";
 import { useDiscussionStore } from "../stores/discussionStore";
@@ -7,18 +8,11 @@ interface Props {
 }
 
 interface State {
+  loading: boolean;
+  status: boolean | undefined;
+
   title: string;
   readme: string;
-
-  edit: {
-    loading: boolean;
-    status: boolean | undefined;
-  }
-
-  create: {
-    loading: boolean;
-    status: boolean | undefined;
-  }
 }
 
 function DiscussionEditor({ id }: Props) {
@@ -32,30 +26,26 @@ function DiscussionEditor({ id }: Props) {
       newState.title = newState.title.substring(0, 100000);
 
     return newState;
-  }, {
-    title: "", readme: "",
-    create: { loading: false, status: undefined },
-    edit: { loading: false, status: undefined },
-  })
+  }, { title: "", readme: "", loading: false, status: undefined });
 
   const queryCreateDiscussion = useDiscussionStore(state => state.queryCreateDiscussion);
   const queryEditDiscussion = useDiscussionStore(state => state.queryEditDiscussion);
 
   const createDiscussion = async () => {
-    if (state.create.loading) return;
+    if (state.loading) return;
 
     if (state.title.length === 0) return;
     if (state.title.length > 100) return;
     if (state.readme.length === 0) return;
     if (state.readme.length > 100000) return;
 
-    setState({ ...state, create: { ...state.create, loading: true, status: undefined } });
+    setState({ ...state, loading: true, status: undefined });
     const status = await queryCreateDiscussion(state.title, state.readme);
-    setState({ ...state, create: { ...state.create, loading: false, status: status } });
+    setState({ ...state, loading: false, status: status });
   }
 
   const editDiscussion = async () => {
-    if (state.edit.loading) return;
+    if (state.loading) return;
     if (!id) return;
 
     if (state.title.length === 0) return;
@@ -63,9 +53,9 @@ function DiscussionEditor({ id }: Props) {
     if (state.readme.length === 0) return;
     if (state.readme.length > 100000) return;
 
-    setState({ ...state, edit: { ...state.edit, loading: true, status: undefined } });
+    setState({ ...state, loading: true, status: undefined });
     const status = await queryEditDiscussion(id, state.title, state.readme);
-    setState({ ...state, edit: { ...state.edit, loading: false, status: status } });
+    setState({ ...state, loading: false, status: status });
   }
 
   const fetchDiscussion = async (id: string) => {
@@ -77,59 +67,41 @@ function DiscussionEditor({ id }: Props) {
     const status = !(!res?.a.data || res.a.error);
     const discussion = res?.a.data;
 
-    return { status, title: discussion?.title, readme: discussion?.readme };
+    return { status, title: discussion?.title ?? "", readme: discussion?.readme ?? "" };
   }
 
   useEffect(() => {
     (async () => {
       if (!id) return;
-      setState({ ...state, edit: { ...state.edit, loading: true, status: undefined } });
+      setState({ ...state, loading: true, status: undefined });
       const out = await fetchDiscussion(id);
-      setState({
-        ...state,
-        title: out.title ?? "",
-        readme: out.readme ?? "",
-        edit: { ...state.edit, loading: false, status: out.status },
-      });
-    })()
-  }, [])
+      setState({ ...state, ...out, loading: false });
+    })();
+  }, []);
 
   return (
-    <>
-      <input
-        type="text"
-        placeholder="title"
-        disabled={id ? state.edit.loading : state.create.loading}
+    <Card shadow="sm" p="lg" m="md" radius="md" withBorder>
+      <TextInput
+        radius="md"
+        placeholder="title..."
         defaultValue={state.title}
         onChange={(ev) => { setState({ ...state, title: ev.target.value }) }}
+        pb="md"
       />
 
-      <br />
-
-      <textarea
-        placeholder="readme"
-        disabled={id ? state.edit.loading : state.create.loading}
+      <Textarea
+        radius="md"
+        placeholder="readme..."
         defaultValue={state.readme}
-        onChange={(ev) => { setState({ ...state, readme: ev.target.value }) }}
-      ></textarea>
+        onChange={(ev) => setState({ ...state, readme: ev.target.value })}
+        autosize
+        pb="md"
+      />
 
-      <br />
-
-      <button
-        disabled={id ? state.edit.loading : state.create.loading}
-        onClick={id ? editDiscussion : createDiscussion}
-      >
-        {id ? <>edit</> : <>create</>}
-      </button>
-
-      {state.create.loading && <><br />loading...</>}
-      {state.create.status === true && <><br />success...</>}
-      {state.create.status === false && <><br />fail...</>}
-
-      {state.edit.loading && <><br />loading...</>}
-      {state.edit.status === true && <><br />success...</>}
-      {state.edit.status === false && <><br />fail...</>}
-    </>
+      <Button onClick={id ? editDiscussion : createDiscussion} color="dark" radius="md">
+        {id ? "edit" : "create"}
+      </Button>
+    </Card>
   )
 }
 
