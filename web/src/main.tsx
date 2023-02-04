5ec558a2992459342b8dcf73a46f520ee9a09093
Berk Cambaz <berkcambaz12321@gmail.com>
2023-02-04 14:41:17 +0300

    Auto refresh pwa

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import Router from './routes/_Router'
 import "./lib/i18n";
+import { registerSW } from 'virtual:pwa-register'
 
+registerSW({ immediate: true })
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<Router />)
