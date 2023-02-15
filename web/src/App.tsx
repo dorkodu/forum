01c87733baa13186ce39f04b166014bf825bee49
Berk Cambaz <berkcambaz12321@gmail.com>
2023-02-15 13:50:32 +0300

    Finally fix theme FART

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -55,7 +55,9 @@ function App() {
   const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
     key: "theme",
     defaultValue: "light",
-    getInitialValueInEffect: true,
+    getInitialValueInEffect: false,
+    serialize: (value) => value,
+    deserialize: (value) => value as ColorScheme,
   });
 
   const toggleColorScheme = (value?: ColorScheme) => {
