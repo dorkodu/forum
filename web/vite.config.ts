e1d92ac872ffa68fbe5d4d45285a13490492864b
Berk Cambaz <berkcambaz12321@gmail.com>
2023-02-17 01:18:57 +0300

    Make icons maskable

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -41,14 +41,16 @@ export default defineConfig({
         background_color: "#000000",
         icons: [
           {
-            src: "/android-chrome-192x192.png",
+            src: "/maskable_icon_x192.png",
             sizes: "192x192",
             type: "image/png",
+            purpose: "any maskable"
           },
           {
-            src: "/android-chrome-512x512.png",
+            src: "/maskable_icon_x512.png",
             sizes: "512x512",
             type: "image/png",
+            purpose: "any maskable"
           },
         ],
       },
