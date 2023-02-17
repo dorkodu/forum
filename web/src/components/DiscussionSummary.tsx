5a36f1796205ee8524a525a2b897d3865790523a
Berk Cambaz <berkcambaz12321@gmail.com>
2023-02-17 21:33:11 +0300

    Fix clicking features in discussion summary

--- web/src/components/DiscussionSummary.tsx
+++ web/src/components/DiscussionSummary.tsx
@@ -7,7 +7,7 @@ import {
   IconStar, IconMessage, IconMessages, IconActivity
 } from "@tabler/icons";
 
-import { ActionIcon, Card, Flex, Text } from "@mantine/core"
+import { ActionIcon, Anchor, Card, Flex, Text } from "@mantine/core"
 import { date } from "../lib/date";
 import { css } from "@emotion/react";
 import { useTranslation } from "react-i18next";
@@ -42,19 +42,25 @@ function DiscussionSummary({ discussionId }: Props) {
   const user = useUserStore(state => state.getUserById(discussion?.userId));
   const currentUserId = useAuthStore(state => state.userId);
 
-  const gotoDiscussion = () => {
+  const gotoDiscussion = (ev: MouseEvent) => {
+    ev.preventDefault()
+
     if (!discussion) return;
     const target = `/discussion/${discussion.id}`;
     if (location.pathname !== target) navigate(target);
   }
 
-  const gotoUser = () => {
+  const gotoUser = (ev: MouseEvent) => {
+    ev.stopPropagation();
+    ev.preventDefault();
+
     if (!user) return;
     navigate(`/profile/${user.username}`);
   }
 
   const favouriteDiscussion = async (ev: MouseEvent) => {
     ev.stopPropagation();
+    ev.preventDefault();
 
     // If user is trying to favourite while not being logged in
     if (!currentUserId) return setRequestLogin(true);
@@ -70,53 +76,55 @@ function DiscussionSummary({ discussionId }: Props) {
   if (!discussion || !user) return (<></>)
 
   return (
-    <Card css={css`overflow: visible;`} shadow="sm" p="lg" m="md" radius="md" withBorder onClick={gotoDiscussion}>
-      <Flex align="center" justify="space-between">
-        <Flex miw={0}>
-          <Flex miw={0} onClick={gotoUser} css={autoGrid}>
-            <Text truncate mr={4}><TextParser text={user.name} types={[PieceType.Emoji]} /></Text>
-            <Text>@</Text>
-            <Text truncate>{user.username}</Text>
+    <Anchor href={`/discussion/${discussion.id}`} variant="text" underline={false} onClick={gotoDiscussion}>
+      <Card css={css`overflow: visible;`} shadow="sm" p="lg" m="md" radius="md" withBorder>
+        <Flex align="center" justify="space-between">
+          <Flex miw={0}>
+            <Flex miw={0} onClick={gotoUser} css={autoGrid}>
+              <Text truncate mr={4}><TextParser text={user.name} types={[PieceType.Emoji]} /></Text>
+              <Text>@</Text>
+              <Text truncate>{user.username}</Text>
+            </Flex>
+            <Text mx={4}>·</Text>
+            <Text css={nowrap} mr={4} title={date(discussion.date).format('lll')}>
+              {date(discussion.date).fromNow()}
+            </Text>
           </Flex>
-          <Text mx={4}>·</Text>
-          <Text css={nowrap} mr={4} title={date(discussion.date).format('lll')}>
-            {date(discussion.date).fromNow()}
-          </Text>
+
+          <DiscussionMenu user={user} discussion={discussion} />
         </Flex>
 
-        <DiscussionMenu user={user} discussion={discussion} />
-      </Flex>
+        <Text css={wrapContent}>
+          <TextParser text={discussion.title} />
+        </Text>
 
-      <Text css={wrapContent}>
-        <TextParser text={discussion.title} />
-      </Text>
-
-      <Flex align="center" gap="xs">
-        <Flex align="center">
-          <ActionIcon color="dark" onClick={favouriteDiscussion}>
-            <IconStar fill={discussion.favourited ? "currentColor" : "none"} />
-          </ActionIcon>
-          <span>{discussion.favouriteCount}</span>
+        <Flex align="center" gap="xs">
+          <Flex align="center">
+            <ActionIcon color="dark" onClick={favouriteDiscussion}>
+              <IconStar fill={discussion.favourited ? "currentColor" : "none"} />
+            </ActionIcon>
+            <span>{discussion.favouriteCount}</span>
+          </Flex>
+          <Flex align="center">
+            <IconMessages />
+            <span>{discussion.argumentCount}</span>
+          </Flex>
+          <Flex align="center">
+            <IconMessage />
+            <span>{discussion.commentCount}</span>
+          </Flex>
+          <Flex align="center">
+            <IconActivity />
+            <span>
+              {discussion.lastUpdateDate === -1 ?
+                t("discussion.never") :
+                date(discussion.lastUpdateDate).fromNow()
+              }
+            </span>
+          </Flex>
         </Flex>
-        <Flex align="center">
-          <IconMessages />
-          <span>{discussion.argumentCount}</span>
-        </Flex>
-        <Flex align="center">
-          <IconMessage />
-          <span>{discussion.commentCount}</span>
-        </Flex>
-        <Flex align="center">
-          <IconActivity />
-          <span>
-            {discussion.lastUpdateDate === -1 ?
-              t("discussion.never") :
-              date(discussion.lastUpdateDate).fromNow()
-            }
-          </span>
-        </Flex>
-      </Flex>
-    </Card>
+      </Card>
+    </Anchor>
   )
 }
 
