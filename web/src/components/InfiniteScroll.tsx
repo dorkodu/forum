81a0548eba47f1c331fe388aea4caf59330f17bc
Berk Cambaz <berkcambaz12321@gmail.com>
2023-03-09 17:13:12 +0300

    Remove pull to refresh

--- web/src/components/InfiniteScroll.tsx
+++ web/src/components/InfiniteScroll.tsx
@@ -1,9 +1,5 @@
 import CardLoader from "./cards/CardLoader";
 import InfiniteScrollComponent from "react-infinite-scroll-component";
-import { Flex, Text } from "@mantine/core";
-import { css } from "@emotion/react";
-import { useTranslation } from "react-i18next";
-import { useEffect, useState } from "react";
 
 interface Props {
   children: React.ReactNode;
@@ -18,55 +14,19 @@ interface Props {
 }
 
 function InfiniteScroll({ children, refresh, next, length, hasMore, hideLoader }: Props) {
-  const { t } = useTranslation();
-
-  const pulldownToRefresh = () => (
-    <Flex direction="row" justify="center" my="md">
-      <Text align="center" css={css`user-select: none;`}>
-        &#8595; {t("pulldownToRefresh")}
-      </Text>
-    </Flex>
-  )
-
-  const releaseToRefresh = () => (
-    <Flex direction="row" justify="center" my="md">
-      <Text align="center" css={css`user-select: none;`}>
-        &#8593; {t("releaseToRefresh")}
-      </Text>
-    </Flex>
-  )
-
-  const [scroll, setScroll] = useState({ y: scrollY, scrolled: false });
-  const onScroll = () => { setScroll({ y: scrollY, scrolled: true }) };
-  const onResize = () => { setScroll({ y: scrollY, scrolled: false }) };
-
-  useEffect(() => {
-    window.addEventListener("scroll", onScroll);
-    window.addEventListener("resize", onResize);
-
-    return () => {
-      window.removeEventListener("scroll", onScroll);
-      window.removeEventListener("resize", onResize);
-    }
-  }, []);
-
   return (
     <InfiniteScrollComponent
       next={next ?? (() => { })}
       refreshFunction={refresh}
 
-      dataLength={length + ((scroll.scrolled && scroll.y <= 0) ? 1 : 0)}
-      hasMore={scroll.scrolled && (scroll.y <= 0 || hasMore)}
+      dataLength={length}
+      hasMore={hasMore}
 
       loader={hideLoader ? null : <CardLoader />}
 
-      pullDownToRefresh
-      pullDownToRefreshThreshold={54}
-      pullDownToRefreshContent={pulldownToRefresh()}
-      releaseToRefreshContent={releaseToRefresh()}
     >
       {children}
-    </InfiniteScrollComponent>
+    </InfiniteScrollComponent >
   )
 }
 
