88c8bd324ed14be89196155648ab639392bd8f5d
Berk Cambaz <berkcambaz12321@gmail.com>
2023-03-22 15:47:39 +0300

    More setup for next.js integration

--- next/next.config.js
+++ next/next.config.js
@@ -1,6 +1,13 @@
+const { i18n } = require('./next-i18next.config')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n,
   reactStrictMode: true,
+  poweredByHeader: false,
+  compiler: {
+    emotion: true,
+  },
 }
 
 module.exports = nextConfig
