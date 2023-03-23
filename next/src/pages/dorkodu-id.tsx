77115d7eee29d06a14bbbb12ba5d36b159911ff2
Berk Cambaz <berkcambaz12321@gmail.com>
2023-03-23 20:28:36 +0300

    Add "dorkodu-id" route

---
+++ next/src/pages/dorkodu-id.tsx
@@ -0,0 +1,23 @@
+import auth from "@/lib/api/controllers/auth";
+import { GetServerSideProps, NextApiRequest, NextApiResponse } from "next";
+
+export default function DorkoduID() { return <></> }
+
+export const getServerSideProps: GetServerSideProps = async (props) => {
+  const req = props.req as NextApiRequest;
+  const res = props.res as NextApiResponse;
+
+  const code = typeof props.query.token === "string" ? props.query.token : undefined;
+
+  if (code) {
+    const input: typeof auth.getAccessToken.arg = { code };
+    await auth.getAccessToken.executor(input, { req, res, shared: {} });
+  }
+
+  return {
+    redirect: {
+      destination: "/",
+      permanent: false,
+    }
+  }
+}
