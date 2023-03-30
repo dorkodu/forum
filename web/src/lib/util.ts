96a1751a4636623a4cc89c6cc4e59ad705f90191
Berk Cambaz <berkcambaz12321@gmail.com>
2023-03-30 21:49:46 +0300

    Generate avatar based on username & id

--- web/src/lib/util.ts
+++ web/src/lib/util.ts
@@ -1,4 +1,5 @@
 import i18n from "./i18n";
+import Identicon from "identicon.js";
 
 function parseUserAgent(ua: string) {
   return ua.split(",").filter(value => value !== "").join(" | ");
@@ -89,6 +90,11 @@ function formatDate(date: number, long?: boolean) {
   else return new Intl.RelativeTimeFormat(i18n.language, { numeric: "auto" }).format(0, "seconds");
 }
 
+function generateAvatar(username: string) {
+  const hash = (username + "0".repeat(16)).substring(0, 16).split("").map(c => c.charCodeAt(0).toString(16).padStart(2, "0")).join("");
+  return `data:image/svg+xml;base64,${new Identicon(hash, { format: "svg" }).toString()}`;
+}
+
 export const util = {
   parseUserAgent,
 
@@ -100,4 +106,6 @@ export const util = {
 
   formatNumber,
   formatDate,
+
+  generateAvatar,
 }
