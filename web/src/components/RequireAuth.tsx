05ec8abf722e17d36ece585b613d231b9130459c
Berk Cambaz <berkcambaz12321@gmail.com>
2023-04-01 15:12:35 +0300

    Better guest experience
    Show request login on entering auth-only routes
    Hide buttons on layout depending on auth status

--- web/src/components/RequireAuth.tsx
+++ web/src/components/RequireAuth.tsx
@@ -1,10 +1,17 @@
+import { useAppStore } from "@/stores/appStore";
+import { useLayoutEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuthStore } from "../stores/authStore"
 
 function RequireAuth() {
   const authorized = useAuthStore(state => state.userId);
 
-  return authorized ? <Outlet /> : < Navigate to="/home" replace />
+  useLayoutEffect(() => {
+    if (authorized) return;
+    useAppStore.getState().setRequestLogin(true);
+  }, []);
+
+  return authorized ? <Outlet /> : <Navigate to="/home" replace />
 }
 
 export default RequireAuth
